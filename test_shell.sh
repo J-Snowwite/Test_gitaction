ec7dddb49314c2184bac27d68c6b499386abb001
JJCA <75126343+J-Snowwite@users.noreply.github.com>
2024-12-10 17:02:41 +0800

    Update test_shell.sh

--- test_shell.sh
+++ test_shell.sh
@@ -1,4 +1,11 @@
 echo "sleep 50S  to test_stop_time"
-sleep 50
 echo "50S" >> 50s.txt
+sleep 50
 cat 50s.txt
+echo "###############"
+pwd
+echo "###############"
+grep "physical id" /proc/cpuinfo | sort | uniq| wc -l
+echo "###############"
+free -h
+echo "###############"
